257ccb3ddac595ba4aa01d661c8e14d50883302a
Rajeev-Cryptographer <183406481+Rajeev-Cryptographer@users.noreply.github.com>
2026-03-03 09:05:36 +0530

    Add URL validation and default to HTTPS

--- Scraper.py
+++ Scraper.py
@@ -3,17 +3,19 @@ from bs4 import BeautifulSoup
 import sys
 
 url= sys.argv[1]
+if not url.startswith("http"):
+    url = "https://" + url
 headers={"User-Agent":"Mozilla/5.0 (Windows NT 10.0; Win64; x64)"}
 res= requests.get(url,headers=headers)
 if(res.status_code!=200):
-    if res.status_code>400 and res.status_code<500:
+    if res.status_code>=400 and res.status_code<500:
         print("issue on client side")
         exit()
     if res.status_code>=500:
         print("issue on server side")
         exit()
 soup=BeautifulSoup(res.text, 'html.parser')
-if soup.head.title:
+if soup.title:
     print(soup.head.title.get_text())
 print('\n')
 if soup.body:
@@ -23,5 +25,6 @@ print('\n')
 links= soup.find_all('a')
 for link in links:
     print(link.get('href'))
+
 
 
